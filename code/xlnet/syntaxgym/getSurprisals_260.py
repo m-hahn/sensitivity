8893b1b00ea1ebbd0262516f0773453d8d4cd09b
Michael Hahn <mhahn29@gmail.com>
2021-04-21 11:09:31 +0800

    code

---
+++ code/xlnet/syntaxgym/getSurprisals_260.py
@@ -0,0 +1,21 @@
+with open("/u/scr/mhahn/PRETRAINED/SyntaxGym/syntaxgym_260_alternatives.tsv", "r") as inFile:
+    alternatives = inFile.read().strip().split("#####\n")
+processed = set()
+
+with open("/u/scr/mhahn/PRETRAINED/SyntaxGym/syntaxgym_260_alternatives_raw.tsv", "w") as outFile:
+  for alt in alternatives:
+    alt = alt.strip().split("\n")[3:]
+    for sent in alt:
+  #     print(sent)
+       subset, sent = sent.split("\t")
+       sent = sent.replace("</s>", "").replace(" ", "").replace("▁", " ").strip()
+       if sent not in processed:
+          processed.add(sent)
+          print(sent, file=outFile)
+          if sent.endswith(" himself."):
+              print(sent.replace(" himself.", " themselves."), file=outFile)
+          elif sent.endswith(" themselves."):
+              print(sent.replace(" themselves.", " himself."), file=outFile)
+          else:
+              assert False
+            
---
+++ code/xlnet/syntaxgym/getSurprisals_260_PMLM.py
@@ -0,0 +1,23 @@
+from nltk.tokenize.treebank import TreebankWordDetokenizer                                         
+detokenizer = TreebankWordDetokenizer()                      
+
+with open("/u/scr/mhahn/PRETRAINED/SyntaxGym/syntaxgym_260_alternatives_PMLM_1billion_raw.tsv", "r") as inFile:
+    alternatives = inFile.read().strip().split("\n")
+processed = set()
+
+with open("/u/scr/mhahn/PRETRAINED/SyntaxGym/syntaxgym_260_alternatives_PMLM_1billion_raw_forGPT2.tsv", "w") as outFile:
+  for alt in alternatives:
+    alt = alt.strip().split("\t")
+    _, _, sent = alt
+    if sent not in processed:
+          processed.add(sent)
+          sent = detokenizer.detokenize( sent.strip().replace("[CLS]", "").split("[SEP]")[0].strip().split(" "))
+          print(sent, file=outFile)
+          if sent.endswith(" himself."):
+              print(sent.replace(" himself.", " themselves."), file=outFile)
+          elif sent.endswith(" themselves."):
+              print(sent.replace(" themselves.", " himself."), file=outFile)
+          else:
+              print(sent, alt)
+              continue
+            
